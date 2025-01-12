1beeed75b8b90b262632c9f09b0ee2f314a6e056
Duanne Moraes <duanne.souza@gmail.com>
2025-01-12 14:17:48 -0300

    feat: Create the login function that generates the token

---
+++ todo_list_pf/routers/auth.py
@@ -0,0 +1,34 @@
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from todo_list_pf.database import get_session
+from todo_list_pf.models import User
+from todo_list_pf.schemas import Token
+from todo_list_pf.security import create_access_token, verify_password
+
+router = APIRouter(prefix="/token", tags=["token"])
+
+T_Session = Annotated[Session, Depends(get_session)]
+T_OAuth2Form = Annotated[OAuth2PasswordRequestForm, Depends()]
+
+
+@router.post("/", response_model=Token)
+def login_for_access_token(
+    session: T_Session,
+    form_data: T_OAuth2Form,
+):
+    user = session.scalar(select(User).where(User.email == form_data.username))
+
+    if not user or not verify_password(form_data.password, user.password):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Incorrect email or password",
+        )
+
+    access_token = create_access_token(data={"sub": user.email})
+
+    return {"access_token": access_token, "token_type": "bearer"}
