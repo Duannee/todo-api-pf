864521f353f78e4265111efdf76e5281b026c515
Duanne Moraes <duanne.souza@gmail.com>
2025-01-12 15:46:24 -0300

    feat: Create the get current user function to validate credentials

--- todo_list_pf/security.py
+++ todo_list_pf/security.py
@@ -1,12 +1,22 @@
 from datetime import datetime, timedelta
+from sqlalchemy.orm import Session
+from fastapi import Depends, HTTPException
+from todo_list_pf.database import get_session
+from http import HTTPStatus
+from todo_list_pf.models import User
+from sqlalchemy import select
 
+
+from fastapi.security import OAuth2PasswordBearer
 import pytz
 from jwt import encode
 from pwdlib import PasswordHash
 
-SECRET_KEY = "0b4b3c1e-0d1e-4f6f-8a7b-2e1f3d7e5c1d"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+from jwt import decode, DecodeError
+from todo_list_pf.settings import Settings
+from todo_list_pf.schemas import TokenData
+
+settings = Settings()
 
 pwd_context = PasswordHash.recommended()
 
@@ -16,11 +26,13 @@ def create_access_token(data: dict):
 
     utc_tz = pytz.utc
 
-    expire_token = datetime.now(utc_tz) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire_token = datetime.now(utc_tz) + timedelta(
+        minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
+    )
 
     to_encode.update({"exp": expire_token})
 
-    encoded_jwt = encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    encoded_jwt = encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
     return encoded_jwt
 
 
@@ -30,3 +42,32 @@ def get_password_hash(password: str):
 
 def verify_password(plain_password: str, hashed_password: str):
     return pwd_context.verify(plain_password, hashed_password)
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+def get_current_user(
+    session: Session = Depends(get_session), token: str = Depends(oauth2_scheme)
+):
+
+    credentials_exception = HTTPException(
+        status_code=HTTPStatus.UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    try:
+        payload = decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        username: str = payload.get("sub")
+        if not username:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except DecodeError:
+        raise credentials_exception
+
+    user = session.scalar(select(User).where(User.email == token_data.username))
+
+    if not user:
+        raise credentials_exception
+    return user
