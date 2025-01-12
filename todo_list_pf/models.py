4badf0aba75ae6ddf91475b3c2f000586ef82ab9
Duanne Moraes <duanne.souza@gmail.com>
2025-01-12 10:49:32 -0300

    feat: Create the task model

--- todo_list_pf/models.py
+++ todo_list_pf/models.py
@@ -1,8 +1,16 @@
 from datetime import datetime
 from sqlalchemy import func
 from sqlalchemy.orm import registry, Mapped, mapped_column
+from enum import Enum
+from sqlalchemy import Enum as SQLAlchemyEnum
 
 register_metadata = registry()
+
+
+class EnumStatus(str, Enum):
+    PENDING = "pending"
+    IN_PROGRESS = "in_progress"
+    COMPLETED = "completed"
 
 
 @register_metadata.mapped_as_dataclass
@@ -15,4 +23,21 @@ class User:
     created_at: Mapped[datetime] = mapped_column(init=False, server_default=func.now())
     updated_at: Mapped[datetime] = mapped_column(
         init=False, server_default=func.now(), onupdate=func.now()
+    )
+
+
+@register_metadata.mapped_as_dataclass
+class Task:
+    __tablename__ = "tasks"
+    id: Mapped[int] = mapped_column(init=False, primary_key=True)
+    title: Mapped[str] = mapped_column(nullable=False)
+    description: Mapped[str] = mapped_column(nullable=True)
+    status: Mapped[EnumStatus] = mapped_column(
+        SQLAlchemyEnum(EnumStatus), nullable=False, default=EnumStatus.PENDING
+    )
+    created_at: Mapped[datetime] = mapped_column(
+        init=False, server_default=func.now(), nullable=False
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        init=False, server_default=func.now(), onupdate=func.now(), nullable=False
     )
