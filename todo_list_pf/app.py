b03d59fae015b304392b6e83fa638ba1fe483a52
Duanne Moraes <duanne.souza@gmail.com>
2025-01-12 21:09:31 -0300

    feat: Create the app.py

---
+++ todo_list_pf/app.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+from todo_list_pf.routers import auth, tasks, users
+
+app = FastAPI()
+
+
+app.include_router(users.router)
+app.include_router(auth.router)
+app.include_router(tasks.router)
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to this fantastic api!"}
