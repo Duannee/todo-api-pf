a8ca3ea2ec36796e5811937b0bc37516b62094ba
Duanne Moraes <duanne.souza@gmail.com>
2025-01-12 22:25:12 -0300

    feat: Create the route responsible for listing tasks

--- todo_list_pf/routers/tasks.py
+++ todo_list_pf/routers/tasks.py
@@ -1,11 +1,13 @@
 from fastapi import APIRouter, Depends
+from sqlalchemy import select
 
-from todo_list_pf.schemas import TaskSchema, TaskPublic
+from todo_list_pf.schemas import TaskSchema, TaskPublic, TaskList
 from todo_list_pf.database import get_session
 from todo_list_pf.models import Task
 from todo_list_pf.security import get_current_user
 from typing import Annotated
 from sqlalchemy.orm import Session
+from todo_list_pf.models import EnumStatus
 
 
 router = APIRouter(prefix="/tasks", tags=["tasks"])
@@ -28,3 +30,22 @@ def create_task(task: TaskSchema, session: T_Session, user: T_User):
     session.refresh(db_task)
 
     return db_task
+
+
+@router.get("/", response_model=TaskList)
+def list_tasks(
+    session: T_Session,
+    user: T_User,
+    status: EnumStatus | None = None,
+    offset: int | None = None,
+    limit: int | None = None,
+):
+
+    query = select(Task).where(Task.user_id == user.id)
+
+    if status:
+        query = query.filter(Task.status == status)
+
+    tasks = session.scalars(query.offset(offset).limit(limit)).all()
+
+    return {"tasks": tasks}
