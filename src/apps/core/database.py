912ca5e9c7282647a3ff55c6754f4252bbd62fcc
Duanne Moraes <duanne.souza@gmail.com>
2025-01-21 14:59:47 -0300

    refactor: Add register metadata in database.py

--- src/apps/core/database.py
+++ src/apps/core/database.py
@@ -2,8 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
 from src.apps.core.settings import Settings
+from sqlalchemy.orm import registry
+
+register_metadata = registry()
 
 engine = create_engine(Settings().DATABASE_URL)
+
+register_metadata.metadata.create_all(engine)
 
 
 def get_session():
